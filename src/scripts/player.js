428dba34f1ac32da214e3c93cf314a099c057463
Miran-Mirantee <killerm11805@hotmail.com>
2023-04-20 00:20:31 +0700

    Add player class

---
+++ src/scripts/player.js
@@ -0,0 +1,20 @@
+class player {
+  constructor() {}
+  randNum() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  randCord() {
+    return new Array(this.randNum(), this.randNum());
+  }
+
+  randHit(missedCord) {
+    let cord = this.randCord;
+    while (missedCord.includes(cord)) {
+      cord = this.randCord();
+    }
+    return cord;
+  }
+}
+
+export default player;
