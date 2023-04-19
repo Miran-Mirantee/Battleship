7fad422f572fccd7f0425bc76081ad3556f9b678
Miran-Mirantee <killerm11805@hotmail.com>
2023-04-19 22:46:44 +0700

    Update cordinates of missed shot and check number of ship on the board

--- src/scripts/gameBoard.js
+++ src/scripts/gameBoard.js
@@ -1,17 +1,32 @@
 import ship from "./ship";
 
 const gameBoard = function () {
-  // option 1
   this.board = new Array(10);
   for (let i = 0; i < 10; i++) {
     this.board[i] = new Array(10);
   }
+  this.missedCord = new Array();
   this.placeShip = (cord, length) => {
     this.board[cord[0]][cord[1]] = new ship(length);
   };
   this.receiveAttack = (cord) => {
-    if (this.board[cord[0]][cord[1]] === undefined) return false;
+    if (this.board[cord[0]][cord[1]] === undefined) {
+      this.missedCord.push(cord);
+      return false;
+    }
+    this.board[cord[0]][cord[1]].hit();
     return true;
+  };
+  this.checkShipNum = () => {
+    let availableShip = 0;
+    for (let i = 0; i < 10; i++) {
+      this.board[i].forEach((ship) => {
+        if (ship.isSunk() === false) {
+          availableShip++;
+        }
+      });
+    }
+    return availableShip;
   };
 };
 
--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -3,6 +3,7 @@ import gameBoard from "../scripts/gameBoard";
 const board = new gameBoard();
 board.placeShip([1, 0], 5);
 board.placeShip([1, 1], 5);
+board.placeShip([4, 3], 5);
 
 test("hit a ship", () => {
   expect(board.receiveAttack([1, 0])).toBeTruthy();
@@ -10,6 +11,10 @@ test("hit a ship", () => {
 
 test("did not hit a ship", () => {
   expect(board.receiveAttack([2, 5])).not.toBeTruthy();
+});
+
+test("check number of ship", () => {
+  expect(board.checkShipNum()).toBe(3);
 });
 
 // test("test", () => {
